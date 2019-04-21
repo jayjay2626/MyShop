228c54e69e59bd3595da5368dd381bc929e204a8
Rangeley Jay Delphin <Rangeleydelphin@gmail.com>
2019-04-21 15:15:33 -0400

    Product Grouping adding

---
+++ MyShop/MyShop.Core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    public class ProductManagerViewModel
+    {
+        // A view model is a special kind of model that use just the view
+        // it is more of a concept than an actual thing
+        // In development it is simply another class.
+        // View models contains data but the data is NOT saved.
+        // View models is simply to transport data between the controller
+        // and the view. Data still have to be manually put in and taken out
+        // each time. We could do this either statically or by copying 
+        // the data through a normal data driven model.
+
+        public Product Product { get; set; }
+
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
