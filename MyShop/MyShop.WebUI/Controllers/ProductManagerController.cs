cfe4450f6c8146b0f0afe2ee3c3bddc7284cc0e4
Rangeley Jay Delphin <Rangeleydelphin@gmail.com>
2019-04-22 10:05:55 -0400

    Adding Unity (Container) Packages

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
@@ -10,18 +11,15 @@ using MyShop.DataAccess.InMemory;
 namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
-    {
-        //ProductRepository context;
-        InMemoryRepository<Product> context;
-
-        //ProductCategoryRepository productCategories;
-        InMemoryRepository<ProductCategory> productCategories;
+    { 
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
 
 
-        public ProductManagerController()
+        public ProductManagerController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
         {
-            context = new InMemoryRepository<Product>();
-            productCategories = new InMemoryRepository<ProductCategory>();
+            context = productContext;
+            productCategories = productCategoryContext;
         }
 
         // GET: ProductManager
